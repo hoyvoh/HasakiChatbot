c746490c096203d3923e4b8dfc0acbe64abe19c2
thaiquanghoctap <thaiquanghoctap@gmail.com>
2024-11-28 23:13:01 +0700

    Update flow 4 - chunked successfully

--- backend/query_processor/minilm.py
+++ backend/query_processor/minilm.py
@@ -35,33 +35,8 @@ def get_decision(query):
     - Giá: Chuyển thành số (ví dụ, 10k thành 10000)
 
     (4) Nếu người dùng cần hỗ trợ hãy trả lời với cú pháp: {'signal':4,'query':'<loại vấn đề cần hỗ trợ>'}.
-    Trong đó loại <loại vấn đề cần hỗ trợ> bắt buộc phải thuộc 1 trong 26 vấn đề dưới đây:
-        1. 'Tài khoản',
-        2. 'Đặt hàng trực tuyến',
-        3. 'Quy cách đóng gói sản phẩm',
-        4. 'Vận chuyển 2H',
-        5. 'Phí vận chuyển',
-        6. 'Đổi trả và hoàn tiền',
-        7. 'Dịch vụ Spa',
-        8. 'Tuyển dụng',
-        9. 'Hướng dẫn đăng ký thành viên Hasaki.',
-        10. 'Có cần đặt lịch trước khi đến spa?',
-        11. 'Tại sao không thể đăng nhập vào tài khoản?',
-        12. 'Cách đặt dịch vụ.',
-        13. 'Sử dụng chung tài khoản với người khác có được không?',
-        14. 'Khám da tại spa Hasaki có tốn phí không?',
-        15. 'Giới thiệu về Hasaki',
-        16. 'Khách hàng thân thiết',
-        17. 'Hướng dẫn đổi quà',
-        18. 'Hướng dẫn mua hàng',
-        19. 'Hướng dẫn đặt hàng 2H',
-        20. 'Hướng dẫn thanh toán trực tuyến',
-        21. 'Thẻ quà tặng Got It',
-        22. 'Phiếu mua hàng',
-        23. 'Chính sách vận chuyển giao nhận',
-        24. 'Điều khoản sử dụng',
-        25. 'Chính sách bảo mật',
-        26. 'Hướng dẫn tải và sử dụng App Hasaki'
+    Trong đó loại <loại vấn đề cần hỗ trợ> bắt buộc phải thuộc 1 trong 37 vấn đề hoặc câu hỏi sau: ['Đăng ký thành viên Hasaki như thế nào?', 'Tại sao tôi không thể đăng nhập vào tài khoản của tôi?', 'Tôi muốn thay đổi thông tin tài khoản thành viên như thế nào?', 'Tôi có thể sử dụng chung tài khoản với người khác được không? ', 'Đăng ký thành viên tại Hasaki.vn sẽ giúp ích gì cho tôi?', 'Hasaki có chương trình ưu đãi nào hấp dẫn dành cho khách hàng thân thiết?', 'Tôi có thể đặt hàng qua điện thoại được không?', 'Có giới hạn về số lượng sản phẩm khi đặt hàng không?', 'Tôi muốn kiểm tra lại đơn hàng đã mua?', 'Tôi muốn thay đổi hoặc hủy bỏ đơn hàng đã mua thì sao?', 'Quy cách đóng gói sản phẩm', 'Giờ làm việc của Hasaki Clinic?', 'Có cần đặt lịch trước khi đến Hasaki Clinic không?', 'Đặt mua phiếu dịch vụ như thế nào?', 'Khám da tại Hasaki Clinic có tốn phí không?', 'Những chi nhánh nào của Hasaki Clinic có bác sĩ da liễu?', 'Hasaki Clinic có chương trình gì hàng tháng?', 'Các dịch vụ thư giãn có dành cho nam không?', 'Mua dịch vụ rồi có hoàn trả được hay không?', 'Tôi cần tư vấn về dịch vụ spa và phòng khám?', 'Tôi có thể sang nhượng dịch vụ cho người khác không?', 'Vận chuyển 2H', 'Phí vận chuyển', 'Đổi trả và hoàn tiền', 'Tuyển dụng', 'Giới thiệu về Hasaki', 'Khách hàng thân thiết', 'Hướng dẫn đổi quà', 'Hướng dẫn đặt hàng', 'Hướng dẫn đặt hàng 2H', 'Hướng dẫn thanh toán trực tuyến', 'Thẻ quà tặng Got It', 'Phiếu mua hàng', 'Chính sách vận chuyển giao nhận', 'Điều khoản sử dụng', 'Chính sách bảo mật', 'Hướng dẫn tải và sử dụng App Hasaki']
+
     Hãy đọc thật kỹ câu hỏi của khách hàng và lựa chọn đúng vấn đề cần hỗ trợ của khách hàng. Phải phân tích kỹ.
     
     (5) Nếu người dùng có ngân sách và cần hỗ trợ mua hàng, trả lời: {'signal':5,'budget':'<ngân sách>','product_term':'<danh sách sản phẩm>'}.
